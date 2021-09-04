1064236155eb5862bc935ce024e0a64ccf414caf
Alina <mlnvccc@gmail.com>
2021-09-04 11:48:47 +0300

    without comments

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const server = express();
 const weather = require("weather-js");
 const fetch = require("node-fetch");
 const { Telegraf } = require("telegraf");
-let counter = 0;
 const PORT = process.env.PORT || 3000;
 const APP_NAME = process.env.APP_NAME;
 
@@ -26,22 +25,21 @@ server.listen(PORT, () => {
   console.log("Server has been started on port ", PORT);
 });
 
-const bot = new Telegraf(process.env.BOT_TOKEN); //сюда помещается токен
+const bot = new Telegraf(process.env.BOT_TOKEN); 
 bot.start((ctx) =>
   ctx.reply(
     `Welcome, ${ctx.message.from.first_name}!\n\nFind out the weather in your city (and get a disney picture as a gift 🎁)`
   )
-); //ответ бота на команду /start
+); 
 bot.help((ctx) =>
   ctx.reply(
     "Write the city you want to know weather in\n\n(Available only in English)"
   )
-); //ответ бота на команду /help
+); 
 bot.on("text", async (ctx) => {
   const response = await fetch("https://api.disneyapi.dev/characters");
   const data = await response.json();
   const random = Math.floor(Math.random() * data.data.length);
-  // counter++;
 
   weather.find(
     { search: `${ctx.message.text}`, degreeType: "C" },
@@ -50,12 +48,6 @@ bot.on("text", async (ctx) => {
         ctx.reply(
           `Weather in ${ctx.message.text} today 🕵🌏:\n${result[0].current.skytext} ${result[0].current.temperature} °C ( feels like ${result[0].current.feelslike} °C)\n\nTomorrow 🔮:\n${result[0].forecast[2].skytextday} ${result[0].forecast[2].low} °C\n\n\nDisney pic for you 🧚‍♀️✨:\n\n${data.data[random].name}\n${data.data[random].imageUrl}`
         );
-        // if (counter === 2) {
-        //   counter = 0;
-        //   ctx.reply(
-        //     "To get information in Russian follow @AnotherLangBot\n\nChoose a movie according to the weather\n@MoviesChoiceBot\n\nIf you forgot the name of the song\n@msc_finder_bot"
-        //   );
-        // }
       } catch (e) {
         ctx.reply(
           `${"https://cs11.pikabu.ru/post_img/2020/05/11/11/1589224804142471795.jpg"}\n\n\nTo get instructions enter /help`
@@ -63,10 +55,10 @@ bot.on("text", async (ctx) => {
       }
     }
   );
-}); //обработчик введенного юзером сообщения
+}); 
 bot.hears("/start", (ctx) =>
   ctx.reply(
     `Welcome, ${ctx.message.from.first_name}!\n\nFind out the weather in your city (and get a disney picture as a gift 🎁)`
   )
-); // обработчик конкретного текста
-bot.launch(); // запуск бота
+); 
+bot.launch(); 
